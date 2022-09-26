68b90f95db2a0449d3021ecb794559a682e18ff7
Daniil Sharenkov <Milkiy174@gmail.com>
2022-09-26 20:23:29 +0300

    feat: beta version of searchbar

--- components/src/components/mainPage/mainPage.tsx
+++ components/src/components/mainPage/mainPage.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import styles from './mainPage.module.css';
 
 const MainPage: React.FC = () => {
-  return <div className={styles.wrapper}></div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.search_wrapper}>
+        <input className={styles.search} type="text" placeholder="Search..." />
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
