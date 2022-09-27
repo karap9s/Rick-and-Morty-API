7365ecc2d1969c617618e5faa685b7f81f09b742
Daniil Sharenkov <Milkiy174@gmail.com>
2022-09-27 23:20:34 +0300

    feat: add about page design

--- components/src/components/about/about.tsx
+++ components/src/components/about/about.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import styles from './about.module.css';
+import Daniil from '../../assets/img/Daniil.jpg';
 
 const About: React.FC = () => {
-  return <div className={styles.wrapper}></div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.card}>
+        <img className={styles.image} src={Daniil} alt="Rick" />
+        <h1 className={styles.name}>Daniil Sharenkov</h1>
+        <h3 className={styles.species}>Species: Human</h3>
+        <h3 className={styles.gender}>Gender: Male</h3>
+        <h3 className={styles.gender}>Status: Alive</h3>
+        <button className={styles.more}>Show More</button>
+      </div>
+    </div>
+  );
 };
 
 export default About;
