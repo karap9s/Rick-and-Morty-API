8205f326c019451616dbe1fea46967248f355337
Daniil Sharenkov <Milkiy174@gmail.com>
2022-09-29 01:07:22 +0300

    feat: test version of componentWillUnmount

--- components/src/components/mainPage/searchBar/searchBar.tsx
+++ components/src/components/mainPage/searchBar/searchBar.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { Component, FormEvent } from 'react';
 import styles from './searchBar.module.css';
 
 const SearchBar: React.FC = () => {
+  class Search extends Component {
+    constructor(props: string) {
+      super(props);
+
+      this.state = {
+        value: '',
+      };
+    }
+
+    handlerInput(event: FormEvent<HTMLInputElement>) {
+      localStorage.setItem('search', JSON.stringify(event.currentTarget.value));
+    }
+
+    componentWillUnmount(): void {
+      this.handlerInput;
+      console.log(JSON.parse(localStorage.getItem('search') || ''));
+    }
+
+    render() {
+      return (
+        <>
+          <input
+            className={styles.search}
+            type="text"
+            placeholder="Search..."
+            onChange={this.handlerInput}
+            defaultValue={JSON.parse(localStorage.getItem('search') || '')}
+          />
+        </>
+      );
+    }
+  }
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.search_wrapper}>
-        <input className={styles.search} type="text" placeholder="Search..." />
+        <Search />
       </div>
     </div>
   );
