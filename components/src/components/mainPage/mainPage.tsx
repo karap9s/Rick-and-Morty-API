fd705df30c99e39c3ff7a3c8e7805733bb5a5708
Daniil Sharenkov <Milkiy174@gmail.com>
2022-10-01 00:05:10 +0300

    fix: remove unnecessary comments

--- components/src/components/mainPage/mainPage.tsx
+++ components/src/components/mainPage/mainPage.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import Cards from './cards/cards';
-import styles from './mainPage.module.css';
-import SearchBar from './searchBar/searchBar';
 
 const MainPage: React.FC = () => {
   return (
